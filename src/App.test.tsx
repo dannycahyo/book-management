63c91507faf4b9d0aa29a56f7cae48758b5cda9a
Danny Dwi Cahyono <dannydwicahyono@gmail.com>
2021-01-28 12:35:50 +0700

    complete-testing-implementation

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -47,4 +47,43 @@ it("should add upcomingbooks and buyed", async () => {
   userEvent.click(screen.getByRole("button", { name: "Submit" }));
 
   userEvent.click(screen.getAllByRole("button", { name: "Done" })[1]);
+
+  userEvent.click(screen.getByRole("link", { name: "My Books" }));
+  expect(history.location.pathname).toEqual("/mybooks");
+
+  userEvent.click(screen.getAllByRole("button", { name: "Open" })[0]);
+  userEvent.click(screen.getByRole("button", { name: "Delete" }));
+
+  userEvent.click(screen.getAllByRole("button", { name: "Open" })[0]);
+  userEvent.click(screen.getByRole("button", { name: "Edit" }));
+
+  userEvent.type(
+    screen.getByRole("textbox", { name: "Title" }),
+    "GRIT & RESSILIENCE"
+  );
+  userEvent.type(screen.getByRole("spinbutton", { name: "Price" }), "130");
+  userEvent.type(
+    screen.getByRole("textbox", { name: "Writer" }),
+    "Angela Heille"
+  );
+  userEvent.type(
+    screen.getByRole("textbox", { name: "Image" }),
+    "https://prodimage.images-bn.com/pimages/9780062316110_p0_v5_s1200x630.jpg"
+  );
+  userEvent.type(
+    screen.getByRole("textbox", { name: "Reason" }),
+    "Just Random Reason"
+  );
+
+  userEvent.click(screen.getByRole("button", { name: "Finish" }));
+  userEvent.click(screen.getByRole("button", { name: "OK" }));
+
+  userEvent.click(screen.getAllByRole("button", { name: "Open" })[0]);
+  userEvent.click(screen.getByRole("button", { name: "Cancel" }));
+
+  const input = screen.getByPlaceholderText("Are You Looking For Some Books ?");
+  const value = "Ego Is The Enemy";
+  userEvent.type(input, value);
+
+  screen.debug(undefined, kMaxLength);
 });
